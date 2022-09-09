338b8a761e2145644dd3bb0cc91c9db693c12d7a
Gabriel Eiki <yokoyama197@gmail.com>
2022-09-09 17:27:46 -0300

    Erro no Pillow

--- AutoAcceptLol.py
+++ AutoAcceptLol.py
@@ -1,4 +1,24 @@
-from cgi import print_form
+import pyautogui
+import time
+
+def check_screen():
+    button_pos = pyautogui.locateOnScreen('button_Aceitar.PNG', confidence=0.7)
+
+    if button_pos != None:
+        pyautogui.click(button_pos)
+        print("partida aceita")
+        return True
+
+    return False
 
 
-print("HELLO WORD")
+def main():
+
+    print("Estou de olho na fila...", end="\n\n")
+    while True:
+        if check_screen():
+            print('Fila Aceita')
+            break
+
+while True:
+    main()
