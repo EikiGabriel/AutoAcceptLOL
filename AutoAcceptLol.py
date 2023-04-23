3229b2df2b96b0907d562a35bbdfcbe34cd449fc
Joel João Medeiros Filho <joel.joao.medeiros@gmail.com>
2023-04-23 14:14:40 -0300

    correções de arquitetura e e adição sleep
    O sleep serve para reduzir o consumo do processamento, e removi a função main porque só adicionou um while a mais

--- AutoAcceptLol.py
+++ AutoAcceptLol.py
@@ -4,22 +4,18 @@ import time
 
 def check_screen():
     button_pos = pyautogui.locateOnScreen('button_Aceitar.PNG', confidence=0.7)
-
+    
     if button_pos != None:
         pyautogui.click(button_pos)
         print("partida aceita")
         return True
-
-    return False
-
-
-def main():
-
-    print("Estou de olho na fila...", end="\n\n")
-    while True:
-        if check_screen():
-            print('Fila Aceita')
-            break
+    else:
+        return False
 
 while True:
-    main()
+    print("Estou de olho na fila...", end="\n\n")
+    if check_screen():
+        print('Fila Aceita')
+            break
+    else:
+        time.sleep(0.2)
